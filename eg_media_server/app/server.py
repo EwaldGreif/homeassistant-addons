578325286efc9883f326f98da6128050a82fcd88
Ewald Greif <ewald.greif@t-online.de>
2025-09-22 00:20:00 +0200

    Test.

--- eg_media_server/app/server.py
+++ eg_media_server/app/server.py
@@ -54,11 +54,12 @@ def serve(req_path):
                     imageCount += 1
                 elif ext.endswith(PLAYLIST_EXTENSIONS):
                     entry_type = "playlist"
-                    with open(abs_path, "r", encoding="utf-8") as file:
-                        data = yaml.safe_load(file)
-                        name = data.get("title", "Unbekannte Playlist")
-                        for video in data.get("playlist", []):
-                            entries.append(video['title'], entry_type, video['src'])
+                    entries.append(name, entry_type, url)
+                    #with open(abs_path, "r", encoding="utf-8") as file:
+                    #    data = yaml.safe_load(file)
+                    #    name = data.get("title", "Unbekannte Playlist")
+                    #    for video in data.get("playlist", []):
+                    #        entries.append(video['title'], entry_type, video['src'])
                             
         return render_template("folder.html", dir=dir, entries=entries, images=imageCount, parent_dir=parent_path)
     else:
